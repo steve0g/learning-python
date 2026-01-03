d2b9b3741e74d2e4f36c7b7600b04d8e0406292e
stegn <gnako0stephane@gmail.com>
2026-01-03 13:27:34 +0000

    Using f-string to display a sentence

---
+++ 01-variables-types/affichage_d_une_phrase.py
@@ -0,0 +1,8 @@
+# Affiche une phrase comme :
+# "Je m'appelle Ali, j'ai 37 ans et je vis à Abidjan." avec la chaîne formatée (f-string)
+
+nom = input("Quel est ton nom ?")
+age = input("Quel est ton age ?")
+ville = input("Où habites-tu ?")
+
+print(f"Je m'appelle {nom}, j'ai {age} ans, et je vis à {ville}")
