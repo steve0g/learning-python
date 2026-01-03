ba18d7289013b8f6a5bd4dbee10f73d7509bde82
stegn <gnako0stephane@gmail.com>
2026-01-03 14:15:50 +0000

    Documentation: addition of an explanatory comment on the scope of variables

--- 01-variables-types/global_variable.py
+++ 01-variables-types/global_variable.py
@@ -8,3 +8,4 @@ myfunc()
 print('Python is ' + x)
 
 # The result is Python is awesome because a variable defined inside a function only exists within that function.
+# A function cannot modify a global variable if we redefine a variable of the same name inside it.
