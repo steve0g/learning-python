443b8aaeec8c255a3b1da62d8bc9b7d657b36056
stegn <gnako0stephane@gmail.com>
2026-01-03 13:14:41 +0000

    Unpacking ou affectation multiple

---
+++ 01-variables-types/multiple_variable_values.py
@@ -0,0 +1,14 @@
+""" Consider the following code
+What will be the result of a"""
+
+# This is called unpacking ou en Français affectation multiple
+fruits = ['apple', 'banana', 'cherry']
+a, b, c = fruits # or fruits = a, b, c
+print(a)
+
+# Python assigne les valeurs de gauche à droite, dans l’ordre de la liste.
+# Donc a reçoit le 1er élément → 'apple'
+# b reçoit le 2e élément → 'banana'
+# c reçoit le 3e élément → 'cherry'
+
+
