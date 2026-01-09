ea1a228d37eed70e33dd07519380536532747951
stegn <gnako0stephane@gmail.com>
2026-01-09 12:09:16 +0000

    Ajout : Affichage de différents types de données

---
+++ 01-variables-types/affichage_de_différents_types_de_données.py
@@ -0,0 +1,10 @@
+# Affichage de différents types de données avec print()
+name = "Steve"
+age = 24
+size = 1.76
+is_student = True
+
+print("Name :", name) # Chaîne de caractères ou str
+print("Age :", age) # Entier ou int
+print("Size :", size) # Float
+print("Student :", is_student) # Booléen ou boolean
