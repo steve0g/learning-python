a180d07f38b4968ebd5d31c5ec679acb3a971f77
stegn <gnako0stephane@gmail.com>
2026-01-12 03:12:43 +0000

    Affichage de variable avec f-string

---
+++ 01-variables-types/declare_et affiche_des_variables.py
@@ -0,0 +1,8 @@
+# 1️⃣ Déclare et affiche des variables
+
+# Crée une variable nom avec ton prénom et une variable age avec ton âge.
+# Affiche-les avec un message formaté.
+
+nom = "Stéphane"
+age = 36
+print(f"Je m'appelle {nom}, et j'ai {age} ans")
