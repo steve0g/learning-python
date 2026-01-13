634a12dab02dba11ae139a31cc91d780956cf5cc
stegn <gnako0stephane@gmail.com>
2026-01-13 18:20:22 +0000

    feat: while - ex02 - compte à rebours

---
+++ 03-boucles/while/ex02_compte_envers.py
@@ -0,0 +1,8 @@
+# EXERCICE 2 — Compter à l’envers ou compte à rebours
+# Objectif : Afficher les nombres de 5 à 1 en utilisant une boucle while.
+
+compteur = 5
+while compteur >= 1:
+    print(compteur)
+    compteur = compteur - 1 # ou compteur -= 1 on décrémente le compteur de 1 à chaque itération
+print("Décollage...")
