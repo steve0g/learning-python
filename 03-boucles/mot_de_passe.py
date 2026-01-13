7465e45173a353f24cbdfd0f284cbf2f9f116371
stegn <gnako0stephane@gmail.com>
2026-01-13 02:43:18 +0000

    Demande de mot de passe avec la boucle while

---
+++ 03-boucles/mot_de_passe.py
@@ -0,0 +1,9 @@
+
+
+mot_de_passe = ""
+# while mot_de_passe != "python123": # Version avec !=
+
+while not mot_de_passe == "Python123": # Version avec not
+    mot_de_passe = input("Veuillez entrer le mot de passe : ")
+print("Mot de passe correct, vous avez accès au compte !")
+
