8af80fa8d644daff020a2b0d2776e466a357811b
stegn <gnako0stephane@gmail.com>
2026-01-13 16:40:19 +0000

    feat: ajout de l'exercice 01 sur la boucle while (compteur 1 à 5)

---
+++ 03-boucles/while/le_compteur_simple.py
@@ -0,0 +1,8 @@
+# EXERCICE 1 : Le compteur simple
+# Objectif : Afficher les nombres de 1 à 5
+
+compteur = 1
+while compteur <= 5:
+    print(compteur)
+    compteur += 1
+print("Fin du comptage.")
