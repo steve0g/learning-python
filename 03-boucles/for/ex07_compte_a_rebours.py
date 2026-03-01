4576f098343fe1309b3dfe8b8bdef6ec6fc52ef3
stegn <gnako0stephane@gmail.com>
2026-03-01 01:09:19 +0000

    feat: utilisation du pas négatif dans range pour un compte à rebours

---
+++ 03-boucles/for/ex07_compte_a_rebours.py
@@ -0,0 +1,11 @@
+"""
+COMPTE À REBOURS
+----------------
+Objectif : Utiliser un pas négatif dans la fonction range.
+Concepts : range(start, stop, step), boucle for.
+"""
+
+for n in range(10, 0, -1):
+    print(f"Lancement dans {n}... ")
+
+print("DÉCOLLAGE !")
