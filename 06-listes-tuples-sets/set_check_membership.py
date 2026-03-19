87a1835310156804b99100a2d16fac8a13d84738
stegn <gnako0stephane@gmail.com>
2026-03-19 22:52:14 +0000

    feat(set): check membership using in

---
+++ 06-listes-tuples-sets/set_check_membership.py
@@ -0,0 +1,13 @@
+"""
+VÉRIFICATION D'UNE VALEUR DANS UN SET - V1.0
+---------------------------------------------
+Ce script illustre les concepts fondamentaux suivants :
+- Vérification de la présence d'un élément dans un set.
+- Utilisation de l'opérateur in.
+"""
+
+numeros = {"0702", "0703"}
+
+# Vérification de la présence d'un élément dans un set.
+"0702" in numeros
+print("0702" in numeros)
