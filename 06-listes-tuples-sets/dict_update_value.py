a0002f1ff1179d4115cae762cad105ac3bab5dd6
stegn <gnako0stephane@gmail.com>
2026-03-19 22:54:18 +0000

    feat(dict): update dictionary value

---
+++ 06-listes-tuples-sets/dict_update_value.py
@@ -0,0 +1,17 @@
+"""
+MODIFICATION D'UNE VALEUR DANS UN DICTIONNAIRE - V1.0
+------------------------------------------------------
+Ce script illustre les concepts fondamentaux suivants :
+- Accès à une valeur via sa clé.
+- Modification d'une valeur existante dans un dictionnaire.
+- Affichage du dictionnaire mis à jour.
+"""
+
+user = {"nom": "Alice", "age": 25}
+
+# Modification d'une valeur
+user["age"] = 30
+user["nom"] = "Stéphane"
+
+# Affichage du dictionnaire mis à jour.
+print(user)
