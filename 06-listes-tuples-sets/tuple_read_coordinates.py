aee31fcdda26dc60475b70667fb110f44d0671cf
stegn <gnako0stephane@gmail.com>
2026-03-19 22:44:04 +0000

    feat(tuple): read values using index

---
+++ 06-listes-tuples-sets/tuple_read_coordinates.py
@@ -0,0 +1,13 @@
+"""
+LECTURE DE COORDONNÉES GPS DEPUIS UN TUPLE - V1.0
+-------------------------------------------------
+Ce script illustre les concepts fondamentaux suivants :
+- Création d'un tuple de coordonnées GPS.
+- Accès aux éléments d'un tuple par index.
+- Affichage des valeurs de latitude et longitude.
+"""
+
+coordonnees = (48.8566, 2.3522)  # Paris, France
+
+# Accès aux éléments par index et affichage des valeurs
+print(coordonnees[0], coordonnees[1])
